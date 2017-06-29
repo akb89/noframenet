a60679be1933b6f242ce315816fc679ea07999dd
akb89 <akb@3azouz.net>
2017-06-29 18:50:08 +0200

    v3.1.0: added check script to check counts of database documents after import

--- config/development.js
+++ config/development.js
@@ -1,9 +1,9 @@
 const logger = require('./../logger/logger');
 
 const config = {
-  dbUri: 'mongodb://localhost:27017/fn_en_d170_dev',
+  dbUri: 'mongodb://localhost:27017/fn_en_d150_dev',
   logger: logger.debug,
-  frameNetDir: '/Users/AKB/Dropbox/FrameNetData/fndata-1.7',
+  frameNetDir: '/Users/AKB/Dropbox/FrameNetData/fndata-1.5',
   frameChunkSize: 100,
   lexUnitChunkSize: 50,
 };
