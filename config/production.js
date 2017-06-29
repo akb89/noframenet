9bea9690f1ce978f9e0b2ce9a1645e639c62647f
akb89 <akb@3azouz.net>
2017-06-29 22:04:15 +0200

    Updated config

--- config/production.js
+++ config/production.js
@@ -1,9 +1,9 @@
 const logger = require('./../logger/logger');
 
 const config = {
-  dbUri: 'mongodb://localhost:27017/fn_en_d170',
+  dbUri: 'mongodb://localhost:27017/fn_en_d150',
   logger: logger.info,
-  frameNetDir: '/Users/AKB/Dropbox/FrameNetData/fndata-1.7',
+  frameNetDir: '/Users/AKB/Dropbox/FrameNetData/fndata-1.5',
   frameChunkSize: 100,
   lexUnitChunkSize: 50,
 };
