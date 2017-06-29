19e737ade52e7f2f58be827412e553b7e961af5e
akb89 <akb@3azouz.net>
2017-06-29 12:22:12 +0200

    Updated dev config

--- config/development.js
+++ config/development.js
@@ -1,9 +1,9 @@
 const logger = require('./../logger/logger');
 
 const config = {
-  dbUri: 'mongodb://localhost:27017/fn_en_d150_dev',
+  dbUri: 'mongodb://localhost:27017/fn_en_d170_dev',
   logger: logger.debug,
-  frameNetDir: '/Users/AKB/Dropbox/FrameNetData/fndata-1.5',
+  frameNetDir: '/Users/AKB/Dropbox/FrameNetData/fndata-1.7',
   frameChunkSize: 100,
   lexUnitChunkSize: 50,
 };
