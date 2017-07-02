65415a71bdd270b550518ece7c898a718f704900
akb89 <akb@3azouz.net>
2017-07-02 15:01:30 +0200

    Updated logger level

--- config/development.js
+++ config/development.js
@@ -2,7 +2,7 @@ const logger = require('./../logger/logger');
 
 const config = {
   dbUri: 'mongodb://localhost:27017/fn_en_d150_dev',
-  logger: logger.info,
+  logger: logger.verbose,
   frameNetDir: '/Users/AKB/Dropbox/FrameNetData/fndata-1.5',
   frameChunkSize: 100,
   lexUnitChunkSize: 200,
