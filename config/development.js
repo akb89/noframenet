a5e909324d615b15ff69a3e70f0c412fdbe4775c
akb89 <akb@3azouz.net>
2017-07-04 15:03:07 +0200

    Updated config dir paths

--- config/development.js
+++ config/development.js
@@ -1,9 +1,9 @@
 const logger = require('./../logger/logger');
 
 const config = {
-  dbUri: 'mongodb://localhost:27017/fn_en_d160_dev',
+  dbUri: 'mongodb://localhost:27017/fn_en_d170_dev',
   logger: logger.verbose,
-  frameNetDir: '/Users/AKB/Dropbox/FrameNetData/fndata-1.6',
+  frameNetDir: '/Users/AKB/Dropbox/FrameNetData/fndata-1.7',
   frameChunkSize: 100,
   lexUnitChunkSize: 200,
 };
