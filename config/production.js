199bcada38a2ab91c71bbdd50b998c2c53e9833b
akb89 <akb@3azouz.net>
2018-01-19 10:31:51 +0100

    Updated config files structure

--- config/production.js
+++ config/production.js
@@ -1,11 +1,11 @@
 const logger = require('./../logger/logger');
 
 const config = {
-  dbUri: 'mongodb://localhost:27017/fn_en_170_dev',
+  dbUri: 'mongodb://localhost:27017/fn_en_170',
   logger: logger.info,
-  frameNetDir: '/Users/AKB/GitHub/dFN/data/fndata-1.7-with-dev',
-  splitsDir: '/Users/AKB/GitHub/dFN/data/fndata-1.7-with-dev/dev',
-  importLexUnits: false,
+  frameNetDir: '/Users/AKB/Dropbox/FrameNetData/fndata-1.7',
+  splitsDir: '/Users/AKB/Dropbox/FrameNetData/fndata-1.7',
+  importLexUnits: true,
   importFullTexts: true,
   frameChunkSize: 150,
   lexUnitChunkSize: 200,
